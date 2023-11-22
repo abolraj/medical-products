3a0c2ead2ff7ee327f2630474d9bd8cbdee1146c
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-22 08:48:20 +0330

    feat - Add env helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,2 +1,13 @@
 <?php
 
+/**
+ * Get the environment variable
+ *
+ * @param string $key
+ * @param mixed $default
+ * @return void
+ */
+function env($key, $default = null){
+    return $_ENV[$key] ?? $default;
+}
+
