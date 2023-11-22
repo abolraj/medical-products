51c69e6829afd55d5d3f9e01968d41014d28a2ac
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-22 09:01:14 +0330

    feat - Handle the constants

--- app/helpers.php
+++ app/helpers.php
@@ -1,13 +1,26 @@
 <?php
 
+// Constants
+
+// Const - Directories
+define('DIR_ROOT', trim(env('APP_ROOT'), '/'));
+define('DIR_APP', DIR_ROOT . '/app');
+define('DIR_CONFIG', DIR_ROOT . '/config');
+define('DIR_DATABASE', DIR_ROOT . '/database');
+define('DIR_PUBLIC', DIR_ROOT . '/public');
+define('DIR_ROUTES', DIR_ROOT . '/routes');
+define('DIR_STORAGE', DIR_ROOT . '/storage');
+define('DIR_VIEWS', DIR_ROOT . '/views');
+
 /**
  * Get the environment variable
  *
  * @param string $key
  * @param mixed $default
- * @return void
+ * @return mixed
  */
-function env($key, $default = null){
+function env($key, $default = null): mixed
+{
     return $_ENV[$key] ?? $default;
 }
 
