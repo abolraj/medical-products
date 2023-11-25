d4337cd36c015512b5501053e0b3f31e2731e461
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-25 13:50:33 +0330

    fix - Handle expired_at fake

--- database/OfferFaker.php
+++ database/OfferFaker.php
@@ -1,6 +1,7 @@
 <?php
 namespace DB\Faker;
 
+use Model\Offer;
 use Model\Product;
 
 class OfferFaker extends Faker {
@@ -8,10 +9,14 @@ class OfferFaker extends Faker {
     {
         $faker = static::create_faker();
         $product_ids = array_column(Product::all(['id']), 'id');
-        
+        $product_ids = array_diff($product_ids, array_column(Offer::all(['product_id']), 'product_id'));
+        if(empty($product_ids))
+            return [];
+
         return [
             'product_id' => $faker->randomElement($product_ids),
             'value' => $faker->numberBetween(5, 99),
+            'expired_at' => $faker->dateTimeBetween('+1 week', '+3 month')->format('Y-m-d H:i:s'),
         ];
     }
 }
