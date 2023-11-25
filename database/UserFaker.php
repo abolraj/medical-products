a0f1b6ef75306f08c343944190c7e4793dd1c1e8
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-25 11:55:49 +0330

    feat - Add UserFaker to generate fake User

---
+++ database/UserFaker.php
@@ -0,0 +1,15 @@
+<?php
+namespace DB\Faker;
+
+class UserFaker extends Faker {
+    public static function generate(): array
+    {
+        $faker = static::create_faker();
+        return [
+            'username' => $faker->userName(),
+            'password' => md5('123456'),
+            'phone' => $faker->phoneNumber(),
+        ];
+    }
+}
+echo "User Faker: Loaded";
