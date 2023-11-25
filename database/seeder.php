52dc3629af460f18d0ecd23d91fddd832c37e271
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-25 11:56:15 +0330

    boot - Add seeder to database to insert records

---
+++ database/seeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use DB\Faker\OfferFaker;
+use DB\Faker\OrderFaker;
+use DB\Faker\ProductFaker;
+use DB\Faker\UserFaker;
+use Model\Offer;
+use Model\Order;
+use Model\Product;
+use Model\User;
+
+// Seed your database entities here
+echo "Seeding ...\n";
+
+// Seed User fake
+echo "User ...";
+for($i = 0; $i < 10; $i++){
+    $data = UserFaker::generate();
+    User::create($data);
+}
+echo "Ok\n";
+
+
+// Seed Product fake
+echo "Product ...";
+for($i = 0; $i < 5; $i++){
+    $data = ProductFaker::generate();
+    Product::create($data);
+}
+echo "Ok\n";
+
+
+// Seed Offer fake
+echo "Offer ...";
+for($i = 0; $i < 10; $i++){
+    $data = OfferFaker::generate();
+    Offer::create($data);
+}
+echo "Ok\n";
+
+
+// Seed Order fake
+echo "Order ...";
+for($i = 0; $i < 20; $i++){
+    $data = OrderFaker::generate();
+    Order::create($data);
+}
+echo "Ok\n";
+
+
+echo "Seeding All Are Done !\n";
