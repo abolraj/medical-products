8668c224bcb2cb2cf6746d8b3285c625d07298d6
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-25 12:41:50 +0330

    fix - Fix the set functionality

--- app/Cache/RedisDriver.php
+++ app/Cache/RedisDriver.php
@@ -1,12 +1,12 @@
 <?php
+
 namespace Cache;
 
 use Predis\Autoloader;
 use Predis\Client;
 
-Autoloader::register();
-
-class RedisDriver extends CacheDriver {
+class RedisDriver extends CacheDriver
+{
     private static Client $client;
     public static function connect(): bool
     {
@@ -15,8 +15,8 @@ class RedisDriver extends CacheDriver {
             'port' => static::$settings['port'],
         ]);
 
-        if($pass = @static::$settings['password']){
-            static::$client -> auth($pass);
+        if ($pass = @static::$settings['password']) {
+            static::$client->auth($pass);
         }
 
         return true;
@@ -24,25 +24,28 @@ class RedisDriver extends CacheDriver {
 
     public static function disconnect(): bool
     {
-        return !!static::$client -> disconnect();
+        return !!static::$client->disconnect();
     }
 
     public static function get($key, $default = null): mixed
     {
-        return @unserialize(static::$client -> get($key)) ?? $default;
+        return @unserialize(static::$client->get($key)) ?? $default;
     }
 
     public static function set($key, $value, $time_to_live = null): bool
     {
         $time_to_live = $time_to_live ?: static::$ttl;
         $s_value = serialize($value);
-        static::$client -> set($key, $s_value, null, $time_to_live);
+        if ($time_to_live)
+            static::$client->setex($key, $time_to_live, $s_value);
+        else
+            static::$client->set($key, $s_value);
 
         return true;
     }
 
     public static function destroy($key): bool
     {
-        return static::$client -> del($key) ;
+        return static::$client->del($key);
     }
 }
