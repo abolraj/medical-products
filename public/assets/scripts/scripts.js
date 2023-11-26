fa5749801af59ec9039b528e98d51890dbbe361a
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 23:34:10 +0330

    fix - Handle products payment

--- public/assets/scripts/scripts.js
+++ public/assets/scripts/scripts.js
@@ -1,4 +1,4 @@
-const root_url = '/'
+const root_url = 'http://localhost:8080'
 $(document).ready($ => {
     // Handle the pay product action
     function pay_product(product_id, user_id, quantity){
@@ -11,20 +11,42 @@ $(document).ready($ => {
                 quantity,
             },
             success(response){
-                $('#product-' + product_id).text(t=>t-=quantity)
-                $('.user-orders-count').text(t=>t+=1)
+                $('#product-' + product_id + ' .quantity').text((i,t)=>t-quantity)
+                $('.user-orders-count').text((i,t)=>+t+1)
+                $('#pay-product-modal').prop('open', false)
+                console.log('success',response)
             },
             error(err, message){
-
+                console.log('error', message, err)
             }
         })
     }
-    $('#list-products .pay-btn').on('click', ()=>{
+    $('#list-products .pay-btn').on('click', function(){
         const product_id = $(this).data('product-id')
         const user_id = $(this).data('user-id')
+        const quantity = $(this).data('quantity')
+        document.getElementById('pay-product-modal').showModal();
+        $('#pay-product-modal .btn-pay').data('product_id', product_id)
+        $('#pay-product-modal .btn-pay').data('user_id', user_id)
+        $('#pay-product-modal .range').attr('max', quantity)
+        $('#pay-product-modal .range-modifiers').empty()
+        for(let i=0; i<quantity; i++){
+            $('<span>').text('|').appendTo('#pay-product-modal .range-modifiers')
+        }
         
     })
 
+    $('#pay-product-modal .range').on('input', function(){
+        $('#pay-product-modal .quantity-count').text($(this).val())
+    })
+
+    // Pay modal
+    $('#pay-product-modal .btn-pay').on('click', function(){
+        const product_id = $(this).data('product_id')
+        const user_id = $(this).data('user_id')
+        const quantity = $('#pay-product-modal .range').val()
+        pay_product(product_id, user_id, quantity)
+    })
 
 
 
