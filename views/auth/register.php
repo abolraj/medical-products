429865746f2a1abb1ebc88ec5930b54a04574105
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 11:57:15 +0330

    feat - Add success message box

--- views/auth/register.php
+++ views/auth/register.php
@@ -1,9 +1,17 @@
-<div class="relative flex flex-col justify-center p-4">
+<div class="relative flex flex-col justify-center min-h-screen  p-4">
     <div class="w-full p-6 m-auto rounded-md shadow-black shadow-md lg:max-w-lg">
         <?php if ($msg = pop_temp_data('error_message')) : ?>
             <div role="alert" class="error alert alert-error mb-10">
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span><?= $msg ?></span>
+            </div>
+        <?php endif; ?>
+        <?php if ($msg = pop_temp_data('success_message')) : ?>
+            <div role="alert" class="error alert alert-success mb-10">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 <span><?= $msg ?></span>
             </div>
