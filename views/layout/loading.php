59e0e72d7de00d873390545cba7264eb53360139
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 10:38:33 +0330

    feat - Add loading view

---
+++ views/layout/loading.php
@@ -0,0 +1,8 @@
+<section id="loader" class="z-50 fixed top-0 right-0 flex w-screen h-screen items-center bg-opacity-70 bg-black justify-center">
+    <span class="loading loading-infinity text-accent w-52 h-52"></span>
+</section>
+<script>
+    $(document).ready(() => {
+        $('#loader').hide()
+    })
+</script>
