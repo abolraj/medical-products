a2173b5dbf0b6a80f1285e608276062960db9c12
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 16:21:45 +0330

    fix - Add the footer

---
+++ views/layout/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer p-10 bg-base-200 text-base-content">
+    <nav>
+        <header class="footer-title">Services</header>
+        <a class="link link-hover">Branding</a>
+        <a class="link link-hover">Design</a>
+        <a class="link link-hover">Marketing</a>
+        <a class="link link-hover">Advertisement</a>
+    </nav>
+    <nav>
+        <header class="footer-title">Company</header>
+        <a class="link link-hover">About us</a>
+        <a class="link link-hover">Contact</a>
+        <a class="link link-hover">Jobs</a>
+        <a class="link link-hover">Press kit</a>
+    </nav>
+    <nav>
+        <header class="footer-title">Legal</header>
+        <a class="link link-hover">Terms of use</a>
+        <a class="link link-hover">Privacy policy</a>
+        <a class="link link-hover">Cookie policy</a>
+    </nav>
+</footer>
+<footer class="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
+    <aside class="items-center grid-flow-col">
+
+        <svg class="svg-icon h-full max-w-full" style="vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <path d="M341.198939 953.37973167c-70.692039 0-141.367705-26.907836-195.184401-80.716347-107.632369-107.624183-107.632369-282.744619 0-390.368802l341.786317-341.786317c51.936883-51.946093 121.254621-80.556711 195.184401-80.55671 73.913407 0 143.230122 28.610618 195.167005 80.55671 51.954279 51.946093 80.556711 121.262808 80.55671 195.184401s-28.602432 143.246495-80.55671 195.184401L536.38334 872.66440867c-53.816696 53.807487-124.492362 80.715323-195.184401 80.715323zM194.597023 824.08192267c80.824817 80.824817 212.379015 80.824817 293.203832 0L829.570799 482.29560667c38.969547-38.969547 60.426231-91.033319 60.426231-146.601916 0-55.56041-21.455661-107.632369-60.426231-146.601916-38.969547-38.96136-91.02411-60.426231-146.585543-60.426231-55.577806 0-107.632369 21.464871-146.601916 60.426231L194.597023 530.87809167c-80.824817 80.84119-80.824817 212.362642 0 293.203831z" fill="#444444" />
+            <path d="M644.648113 592.67759967l-36.440957 36.440957c-13.361309 13.361309-35.226293 13.361309-48.587602 0L389.564483 459.06246267c-13.416568-13.416568-13.416568-35.170011 0-48.587602l36.440957-36.440957 218.642673 218.643696z" fill="#00D8A0" />
+        </svg>
+        <p>Medical Products - TSH <br />Developed By
+            <a href="linkedin.com/abolfazl-rajaee-nasab" class="text-primary">Abolfazl Rajaee Nasab</a> &copy; 2023
+        </p>
+    </aside>
+    <nav class="md:place-self-center md:justify-self-end">
+        <div class="grid grid-flow-col gap-4">
+            <a href="https://github.com/abolraj/medical-products">
+                <svg width="24" height="24" style="vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M512 95.872a426.666667 426.666667 0 0 0-134.912 831.445333c21.333333 3.754667 29.312-9.045333 29.312-20.266666 0-10.112-0.512-43.733333-0.512-79.445334-107.221333 19.712-134.954667-26.154667-143.488-50.133333a155.136 155.136 0 0 0-43.733333-60.288c-14.933333-7.978667-36.266667-27.733333-0.554667-28.245333a85.376 85.376 0 0 1 65.621333 43.733333 91.178667 91.178667 0 0 0 124.245334 35.2 89.770667 89.770667 0 0 1 27.221333-57.088c-94.933333-10.666667-194.133333-47.445333-194.133333-210.645333a166.058667 166.058667 0 0 1 43.733333-114.688 153.344 153.344 0 0 1 4.266667-113.066667s35.712-11.178667 117.333333 43.733333a402.218667 402.218667 0 0 1 213.333333 0c81.578667-55.466667 117.333333-43.733333 117.333334-43.733333a153.301333 153.301333 0 0 1 4.266666 113.066667 165.077333 165.077333 0 0 1 43.733334 114.688c0 163.712-99.754667 199.978667-194.688 210.645333a101.034667 101.034667 0 0 1 28.8 78.933333c0 57.088-0.512 102.954667-0.512 117.333334 0 11.221333 7.978667 24.533333 29.312 20.266666A426.88 426.88 0 0 0 512 95.872z" />
+                </svg> 
+            </a>
+        </div>
+    </nav>
+</footer>
