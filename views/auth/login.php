71b04df99274e5c5c62f6262a50c463a9858f5d6
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 10:37:37 +0330

    feat  - Add login view

---
+++ views/auth/login.php
@@ -0,0 +1,31 @@
+<div class="relative flex flex-col justify-center h-screen overflow-hidden">
+    <div class="w-full p-6 m-auto rounded-md shadow-lg lg:max-w-lg">
+        <?php if ($msg = pop_temp_data('error_message')) : ?>
+            <div role="alert" class="error alert alert-error mb-10">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span><?= $msg ?></span>
+            </div>
+        <?php endif; ?>
+        <h1 class="text-3xl font-semibold text-center text-primary mb-2"> Login | Welcome back ! </h1>
+        <form class="space-y-4" action="<?= url('auth.signin') ?>" method="post">
+            <div>
+                <label class="label" for="username">
+                    <span class="text-base label-text">User Name</span>
+                </label>
+                <input type="text" name="username" placeholder="User Name" id="username" class="w-full input input-bordered input-primary" required />
+            </div>
+            <div>
+                <label class="label" for="password">
+                    <span class="text-base label-text">Password</span>
+                </label>
+                <input type="password" name="password" placeholder="Enter Password" minlength="6" id="password" class="w-full input input-bordered input-primary" required />
+            </div>
+            <a href="#" class="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
+            <div>
+                <button class="btn btn-primary w-full">Login</button>
+            </div>
+        </form>
+    </div>
+</div>
