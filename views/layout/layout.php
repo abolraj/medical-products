98be6ffb8c5f28a6715dba8a66ebcf18534f55c6
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 08:00:19 +0330

    feat - Add layout page

--- views/layout/layout.php
+++ views/layout/layout.php
@@ -1,28 +1,12 @@
-<!DOCTYPE html>
-<html lang="en" data-theme="<?=env('APP_THEME','light')?>">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= env('APP_DESCRIPTION', 'App Description') ?>">
-    <title><?= env('APP_TITLE', 'App Title') ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.4.7/dist/full.min.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com" defer></script>
-</head>
-
-<body>
-    <header>
-        <?= view('layout/header') ?>
-    </header>
-    <main>
-        <?= view($_main_path ?? 'layout/main') ?>
-    </main>
-    <aside>
-        <?= view('layout/sidebar') ?>
-    </aside>
-    <footer>
-        <?= view('layout/footer') ?>
-    </footer>
-</body>
-
-</html>
+<header>
+    <?= view('layout/header') ?>
+</header>
+<main>
+    <?= view($_main_path ?? 'layout/main') ?>
+</main>
+<aside>
+    <?= view('layout/sidebar') ?>
+</aside>
+<footer>
+    <?= view('layout/footer') ?>
+</footer>
