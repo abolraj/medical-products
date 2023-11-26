7a539cf890ec7c43fe5acccfb19f177c732234c0
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 07:59:45 +0330

    fix - Handle 404 Not found page functionalities

--- routes/routes.php
+++ routes/routes.php
@@ -1,14 +1,21 @@
 <?php
 
+use Pecee\Http\Request;
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
 // Add your routes here
 
 
 
+Router::get('/404', function(){
+    render('layout/404', [], 0);
+})->name('404');
 
-
-
+Router::error(function(Request $request, \Exception $e){
+    if($e->getCode() === 404){
+        response()->redirect(url('404'));
+    }
+});
 
 // Start routing
 Router::start();
---
+++ views/layout/404.php
@@ -0,0 +1,5 @@
+<section class="fixed h-[100vh] w-[100vw] flex items-center justify-center">
+    <h2 class="text-4xl fixed">
+        404 | Not Found
+    </h2>
+</section>
