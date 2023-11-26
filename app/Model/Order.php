edd1e31b3077aebc742b206338cba684250a42d1
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-26 20:57:04 +0330

    feat - Add take order functionalities

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -1,5 +1,27 @@
 <?php
+
 namespace Model;
-class Order extends Model {
+
+class Order extends Model
+{
     protected static string $table = 'orders';
+
+    public static function take_order($product, $user, $quantity): array|bool
+    {
+        if ($product['quantity'] < $quantity) {
+            return false;
+        }
+        $product['quantity'] -= $quantity;
+
+        static::create([
+            'product_id' => $user['id'],
+            'user_id' => $product['id'],
+            'quantity' => $quantity,
+            'price' => $product['price'],
+            'total' => $product['price'] * $quantity,
+            'is_paid' => 0,
+        ]);
+
+        Product::update(['quantity' => $product['quantity']], ["`id` = '".$product['id']."'"]);
+    }
 }
