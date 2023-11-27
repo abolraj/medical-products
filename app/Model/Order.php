b40e1037bd7891c3edd8c5f51526eacae748d462
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 16:35:13 +0330

    feat - Add Order cancel order functionalities

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -15,13 +15,13 @@ class Order extends Model
         }
         $product['quantity'] -= $quantity;
 
-        try{
-            if($offer = Offer::take_offer($user['id'], $product['id'])){
+        try {
+            if ($offer = Offer::take_offer($user['id'], $product['id'])) {
                 $offer_value = +$offer['value'];
-                $product['price'] = ceil( $product['price'] * (100 - $offer_value) / 100 );
+                $product['price'] = ceil($product['price'] * (100 - $offer_value) / 100);
             }
             report('Take Order With Offer', $offer);
-    
+
             static::create([
                 'product_id' => $product['id'],
                 'user_id' => $user['id'],
@@ -30,14 +30,53 @@ class Order extends Model
                 'total_price' => $product['price'] * $quantity,
                 'is_paid' => 0,
             ]);
-    
-            Product::update(['quantity' => $product['quantity']], ["`id` = '".$product['id']."'"]);
-    
+
+            Product::update(['quantity' => $product['quantity']], ["`id` = '" . $product['id'] . "'"]);
+
             return true;
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             report('Error Take Order', $e->getMessage());
             return false;
         }
+    }
 
+    public static function cancel_order($order_id): array|bool
+    {
+        try {
+            $order = static::find($order_id);
+            if (!$order)
+                return $order;
+            $user_id = $order['user_id'];
+            $product_id = $order['product_id'];
+            // Make related offer not consumed
+            Offer::update(
+                [
+                    'consumed_at' => null
+                ],
+                [
+                    "`user_id` = '$user_id'",
+                    "`product_id` = '$product_id'",
+                ]
+            );
+    
+            // Give quantity to product
+            $product = Product::find($product_id);
+            Product::update(
+                [
+                    'quantity' => $product['quantity'] + $order['quantity']
+                ],
+                [
+                    "`id` = '$product_id'",
+                ]
+            );
+    
+            // Delete the order
+            static::delete(["id = `$order_id`"]);  
+            
+            return $order;
+        } catch (\Exception $e) {
+            report('Error Cancel Order', $e->getMessage());
+            return false;
+        }
     }
 }
