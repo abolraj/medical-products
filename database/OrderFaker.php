8491fc058a86840eb085032b788590857958163e
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 16:12:47 +0330

    fix - Add the OrderFaker offer functionalities

--- database/OrderFaker.php
+++ database/OrderFaker.php
@@ -1,6 +1,7 @@
 <?php
 namespace DB\Faker;
 
+use Model\Offer;
 use Model\Order;
 use Model\Product;
 use Model\User;
@@ -11,10 +12,17 @@ class OrderFaker extends Faker {
         $faker = static::create_faker();
         $product_ids = array_column(Product::all(['id']), 'id');
         $user_ids = array_column(User::all(['id']), 'id');
+        $user_id = $faker->randomElement($user_ids);
         $product_id = $faker->randomElement($product_ids);
         $product = Product::find($product_id);
         $max_quantity = $product['quantity'];
         $quantity = $faker->numberBetween(1, $max_quantity);       
+
+        // Handle if there is an offer
+        if($offer = Offer::take_offer($user_id, $product_id)){
+            $offer_value = +$offer['value'];
+            $product['price'] = ceil( $product['price'] * (100 - $offer_value) / 100 );
+        }
 
         return [
             'quantity' => $quantity,
@@ -22,7 +30,7 @@ class OrderFaker extends Faker {
             'total_price' => $quantity * $product['price'],
             'is_paid' => $faker->boolean(60),
             'product_id' => $faker->randomElement($product_ids),
-            'user_id' => $faker->randomElement($user_ids),
+            'user_id' => $user_id,
         ];
     }
 }
