cbb17ebf8413f6fe76e8aaa8dd72df26781572b4
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 16:09:55 +0330

    feat - Add Offer method functionalities

--- app/Model/Offer.php
+++ app/Model/Offer.php
@@ -1,6 +1,42 @@
 <?php
+
 namespace Model;
-class Offer extends Model {
+
+class Offer extends Model
+{
     protected static string $table = 'offers';
 
+    public static function offer($user_id, $product_id): array|bool{
+        return static::read(
+            ['*'],
+            [
+                "`user_id` = '$user_id'",
+                "`product_id` = '$product_id'",
+            ]
+        );
+    }
+    
+    public static function take_offer($user_id, $product_id): array|bool
+    {
+        $offer = static::offer($user_id, $product_id);
+        // Handle if there is an offer
+        if ($offer) {
+            $offer = $offer[0];
+            report(json_encode($offer), 'Take Offer');
+            if (!$offer['consumed_at'] && strtotime($offer['expired_at']) >= time()) {
+                static::update(
+                    [
+                        'consumed_at' => date('Y-m-d H:i:s', time()),
+                    ],
+                    [
+                        "`id` = '".$offer['id']."'"
+                    ]
+                );
+                return $offer;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 }
