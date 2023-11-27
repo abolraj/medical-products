8ffde2ff7d79d437c5fd13853897f3de3b91f232
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 07:04:31 +0330

    fix - Handle when no user logged

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,45 +1,61 @@
 <header>
     <div class="navbar bg-base-200">
         <div class="flex-1">
-            <a class="btn btn-ghost rounded-md text-xl" href="<?=url('home')?>">
-                <?=env('APP_NAME', 'App Name')?>
+            <a class="btn btn-ghost rounded-md text-xl" href="<?= url('home') ?>">
+                <?= env('APP_NAME', 'App Name') ?>
             </a>
         </div>
         <div class="flex-none">
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle">
-                    <div class="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                        <?php if($orders = user_orders()): 
-                            $unpaid_orders = array_filter($orders, fn($order)=>!$order['is_paid']);
+            <?php if ($orders = user_orders()) : ?>
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost btn-circle">
+                        <div class="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                            <?php
+                            $unpaid_orders = array_filter($orders, fn ($order) => !$order['is_paid']);
                             $total_price = array_sum(array_column($unpaid_orders, 'total_price'))
                             ?>
-                            <span class="user-orders-count badge badge-sm indicator-item rounded-full"><?=count($unpaid_orders)?></span>
-                        <?php endif;?>
-                    </div>
-                </label>
-                <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-                    <div class="card-body">
-                        <span class="font-bold text-lg"><?=count($unpaid_orders)?> Items</span>
-                        <span class="text-info">Total: <span class="user-orders-count"><?=$total_price?></span>$</span>
-                        <div class="card-actions">
-                            <button class="btn btn-primary btn-block">View cart</button>
+                            <span class="user-orders-count badge badge-sm indicator-item rounded-full"><?= isset($unpaid_orders) ? count($unpaid_orders) : '' ?></span>
+                        </div>
+                    </label>
+                    <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                        <div class="card-body">
+                            <span class="font-bold text-lg"><?= isset($unpaid_orders) ? count($unpaid_orders) : '' ?> Items</span>
+                            <span class="text-info">Total: <span class="user-orders-count"><?= $total_price ?></span>$</span>
+                            <div class="card-actions">
+                                <button class="btn btn-primary btn-block">View cart</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost rounded-full">
-                    <h2>
-                        <?=user('username')?>
-                    </h2>
-                </label>
-                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Logout</a></li>
-                </ul>
-            </div>
+            <?php endif; ?>
+            <?php if ($orders) : ?>
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost rounded-full">
+                        <h2>
+                            <?= user('username') ?>
+                        </h2>
+                    </label>
+                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="<?= url('auth.logout') ?>">Logout</a></li>
+                    </ul>
+                </div>
+            <?php else : ?>
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost rounded-full">
+                        <h2>
+                            Login / Register
+                        </h2>
+                    </label>
+                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="<?= url('auth.login') ?>">Login</a></li>
+                        <li><a href="<?= url('auth.register') ?>">Register</a></li>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
         </div>
     </div>
 </header>
