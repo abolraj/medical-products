11555f51b72c77dfecce4ff3aec59592b00f11bd
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 16:15:29 +0330

    feat - Handle price and offer monitoring in addition modal

--- public/assets/scripts/scripts.js
+++ public/assets/scripts/scripts.js
@@ -26,10 +26,21 @@ $(document).ready($ => {
         const product_id = $(this).data('product-id')
         const user_id = $(this).data('user-id')
         const quantity = $(this).data('quantity')
+        const offer = $(this).data('product-offer')
+        const price = $(this).data('product-price')
         document.getElementById('pay-product-modal').showModal();
         $('#pay-product-modal .btn-pay').data('product_id', product_id)
         $('#pay-product-modal .btn-pay').data('user_id', user_id)
+        $('#pay-product-modal .btn-pay').data('price', price)
         $('#pay-product-modal .range').attr('max', quantity)
+        if(offer){
+            $('#pay-product-modal .btn-pay').data('offer', offer)
+            $('#pay-product-modal .badge-offer').show()
+            $('#pay-product-modal .badge-offer .offer-value').text(offer)
+        }else{
+            $('#pay-product-modal .badge-offer').hide()
+        }
+        $('#pay-product-modal .badge-price .price-value').val(offer)
         $('#pay-product-modal .range-modifiers').empty()
         for(let i=0; i<quantity; i++){
             $('<span>').text('|').appendTo('#pay-product-modal .range-modifiers')
@@ -38,7 +49,16 @@ $(document).ready($ => {
     })
 
     $('#pay-product-modal .range').on('input', function(){
-        $('#pay-product-modal .quantity-count').text($(this).val())
+        const quantity = $(this).val()
+        let price = $("#pay-product-modal .btn-pay").data('price')
+        const offer = $("#pay-product-modal .btn-pay").data('offer')
+        if(!!offer){
+            price = Math.ceil(price * (100 - offer) / 100)
+        }
+        price *= quantity
+        $('#pay-product-modal .quantity-count').text(quantity)
+        $('#pay-product-modal .badge-price .price-value').text(price)
+
     })
 
     // Pay modal
--- views/products/list.php
+++ views/products/list.php
@@ -2,19 +2,21 @@
     <div class="modal-box">
         <h3 class="font-bold text-lg">
             Take Your Order !
-            <span class="badge badge-secondary">+ <span class="quantity-count">1</span></span>
+            <span class="badge badge-secondary inline-block">+ <span class="quantity-count">1</span></span>
+            <span class="badge-price badge badge-secondary inline-block"><span class="price-value">1</span> $</span>
+            <span class="badge-offer badge badge-error inline-block hidden">+ <span class="offer-value">1</span> % OFF</span>
         </h3>
         <p class="py-4">Please select the quantity you want to pay !</p>
 
-        <input class="range range-secondary" type="range" step="1" min="1" max="10" value="1"/>
+        <input class="range range-secondary" type="range" step="1" min="1" max="10" value="1" />
         <div class="range-modifiers text-secondary w-full flex justify-between text-xs px-2">
         </div>
 
         <label class="label">Phone number :</label>
-        <input type="text" class="input input-secondary" value="<?=user('phone')?>" readonly/>
+        <input type="text" class="input input-secondary" value="<?= user('phone') ?>" readonly />
 
         <div class="modal-action">
-            <button class="btn-pay btn btn btn-success">Pay It !</button>
+            <button class="btn-pay btn btn btn-success">Add To Cart !</button>
             <form method="dialog">
                 <!-- if there is a button in form, it will close the modal -->
                 <button class="btn btn-secondary">Close</button>
@@ -28,8 +30,13 @@
     <ul id="list-products" class="flex flex-wrap gap-2">
         <?php foreach ($products as $product) : ?>
             <li id="product-<?= $product['id'] ?>" class="card w-80 grow bg-base-100 shadow-xl">
-                <figure>
+                <figure class="relative">
                     <img class="object-cover w-full h-full" src="<?= path($product['image'], '/images') ?>" alt="Products - <?= $product['name'] ?>" />
+                    <?php if ($offers[$product['id']] ?? null) : ?>
+                        <div class="absolute opacity-90 bg-base-100 text-error rounded-tr-lg p-4 left-0 bottom-0">
+                            <?= $offers[$product['id']]['value'] ?>% <span class="">OFF</span>
+                        </div>
+                    <?php endif; ?>
                 </figure>
                 <div class="card-body">
                     <h2 class="card-title flex flex-wrap">
@@ -40,7 +47,7 @@
                     <p><?= $product['description'] ?></p>
                     <div class="card-actions justify-end">
                         <?php if ($user) : ?>
-                            <button class="pay-btn btn btn-primary" data-user-id="<?= $user['id'] ?>" data-product-id="<?= $product['id'] ?>" data-quantity="<?= $product['quantity'] ?>">Add To Cart</button>
+                            <button class="pay-btn btn btn-primary" data-product-price="<?=$product['price']?>" data-product-offer="<?=$offers[$product['id']]['value'] ?? 0?>" data-user-id="<?= $user['id'] ?>" data-product-id="<?= $product['id'] ?>" data-quantity="<?= $product['quantity'] ?>">Add To Cart</button>
                         <?php endif; ?>
                     </div>
                 </div>
