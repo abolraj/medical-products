afdcc9296fdee782ed19f032a671b842a437c806
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 09:22:55 +0330

    feat - Add content to main view

--- views/layout/main.php
+++ views/layout/main.php
@@ -1,3 +1,6 @@
-<main class="min-h-screen">
-
+<main class="min-h-screen w-full text-center box-border p-10 text-3xl">
+    <h1 class="mt-24">Welcome To Shop of Medical Products, 
+        <br>
+        You can look for products and choose anyone anycount !
+    </h1>
 </main>
