638ae4f62c4568d90b3caff6861c8c6f43d87a6d
abolraj <142489829+abolraj@users.noreply.github.com>
2023-11-27 16:13:28 +0330

    fix - Change parameters

--- database/ProductFaker.php
+++ database/ProductFaker.php
@@ -5,11 +5,12 @@ class ProductFaker extends Faker {
     public static function generate(): array
     {
         $faker = static::create_faker();
-        $image = store($faker->image(), '/images');
+        $image = 'https://d12oja0ew7x0i8.cloudfront.net/images/Article_Images/ImageForArticle_22159_16692792485341491.jpg';
+        $image = store($image, '/images');
         return [
             'name' => 'Product - ' . $faker->numerify('####'),
             'description' => $faker->text(),
-            'price' => $faker->numberBetween(100_000, 10_000_000),
+            'price' => $faker->numberBetween(1, 100)*10,
             'quantity' => $faker->numberBetween(0, 40),
             'image' => $image,
         ];
